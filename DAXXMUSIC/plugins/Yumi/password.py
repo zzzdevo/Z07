743d4d38469ac45c2de25ae6e1219c4a8e797df9
zzzdevo <110237161+zzzdevo@users.noreply.github.com>
2024-01-01 16:14:08 +0300

    Update password.py

--- DAXXMUSIC/plugins/Yumi/password.py
+++ DAXXMUSIC/plugins/Yumi/password.py
@@ -1,12 +1,13 @@
 import random, os
 from pyrogram import Client, filters, enums 
+from strings.filters import command
 from DAXXMUSIC import app
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
-@app.on_message(filters.command(["genpassword", 'genpw']))
+@app.on_message(command(["genpassword", 'genpw',"پاسوۆرد","پاسورد"]))
 async def password(bot, update):
-    message = await update.reply_text(text="Pʀᴏᴄᴇꜱꜱɪɴɢ..")
+    message = await update.reply_text(text="** پڕۆسەی دەکات..**")
     password = "abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+".lower()
     if len(update.command) > 1:
         qw = update.text.split(" ", 1)[1]
@@ -15,6 +16,6 @@ async def password(bot, update):
         qw = random.choice(ST)
     limit = int(qw)
     random_value = "".join(random.sample(password, limit))
-    txt = f"<b>Lɪᴍɪᴛ:</b> {str(limit)} \n<b>Pᴀꜱꜱᴡᴏʀᴅ: <code>{random_value}</code>"
-    btn = InlineKeyboardMarkup([[InlineKeyboardButton('𝗔𝗗𝗗 𝗠𝗘', url='https://t.me/YumikooBot?startgroup=true')]])
+    txt = f"<b>سنووردار:</b> {str(limit)} \n<b>پاسوۆرد: </b><code>{random_value}</code>"
+    btn = InlineKeyboardMarkup([[InlineKeyboardButton('𝗔𝗗𝗗 𝗠𝗘', url='https://t.me/IQMCBOT?startgroup=true')]])
     await message.edit_text(text=txt, reply_markup=btn, parse_mode=enums.ParseMode.HTML)
