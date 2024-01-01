8098e98ab3bf552dbf73ef60b9e7e2ad7e810033
zzzdevo <110237161+zzzdevo@users.noreply.github.com>
2024-01-01 17:41:45 +0300

    Update join.py

--- DAXXMUSIC/plugins/Yumi/join.py
+++ DAXXMUSIC/plugins/Yumi/join.py
@@ -2,7 +2,7 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
 from config import SUPPORT_IQ
-from YukkiMusic import app
+from DAXXMUSIC import app
 
 @app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(bot: Client, msg: Message):
