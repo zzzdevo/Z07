8e49b74d85490daf834d6f5da6601b25a1c2313d
zzzdevo <110237161+zzzdevo@users.noreply.github.com>
2024-01-01 03:15:29 +0300

    Update config.py

--- config.py
+++ config.py
@@ -41,7 +41,7 @@ UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
     "https://github.com/zzzdevo/Z07",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
