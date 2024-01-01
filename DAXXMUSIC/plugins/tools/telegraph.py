36e2dbf3e9a6be16c82c8755b50694164be9ce08
zzzdevo <110237161+zzzdevo@users.noreply.github.com>
2024-01-01 14:08:27 +0300

    Update telegraph.py

--- DAXXMUSIC/plugins/tools/telegraph.py
+++ DAXXMUSIC/plugins/tools/telegraph.py
@@ -2,19 +2,19 @@ from telegraph import upload_file
 from pyrogram import filters
 from DAXXMUSIC import app
 from pyrogram.types import InputMediaPhoto
+from strings.filters import command
 
-
-@app.on_message(filters.command(["tgm" , "link"]))
+@app.on_message(command(["/tgm" , "تێلەگراف", "تیلەگراف"]))
 def ul(_, message):
     reply = message.reply_to_message
     if reply.media:
-        i = message.reply("𝐌𝙰𝙺𝙴 𝐀 𝐋𝙸𝙽𝙺...")
+        i = message.reply("**𝐌𝙰𝙺𝙴 𝐀 𝐋𝙸𝙽𝙺...**")
         path = reply.download()
         fk = upload_file(path)
         for x in fk:
             url = "https://telegra.ph" + x
 
-        i.edit(f'Yᴏᴜʀ ʟɪɴᴋ sᴜᴄᴄᴇssғᴜʟ Gᴇɴ {url}')
+        i.edit(f'**•⎆┊لینکی میدیا دروست کرا🕷** {url}')
 
 ########____________________________________________________________######
 
@@ -22,10 +22,10 @@ def ul(_, message):
 def ul(_, message):
     reply = message.reply_to_message
     if reply.media:
-        i = message.reply("𝐌𝙰𝙺𝙴 𝐀 𝐋𝙸𝙽𝙺...")
+        i = message.reply("**𝐌𝙰𝙺𝙴 𝐀 𝐋𝙸𝙽𝙺...**")
         path = reply.download()
         fk = upload_file(path)
         for x in fk:
             url = "https://graph.org" + x
 
-        i.edit(f'Yᴏᴜʀ ʟɪɴᴋ sᴜᴄᴄᴇssғᴜʟ Gᴇɴ {url}')
+        i.edit(f'**•⎆┊لینکی میدیا دروست کرا🕷**  {url}')
