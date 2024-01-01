d00bedb49045ac3e2466a5467dbe2e31c281d332
zzzdevo <110237161+zzzdevo@users.noreply.github.com>
2024-01-01 16:16:11 +0300

    Update time.py

--- DAXXMUSIC/plugins/Yumi/time.py
+++ DAXXMUSIC/plugins/Yumi/time.py
@@ -1,15 +1,16 @@
 from pyrogram import Client, filters
 from datetime import datetime
 import pytz
+from strings.filters import command
 from DAXXMUSIC import app
 
 
 def get_current_time():
-    tz = pytz.timezone('Asia/Kolkata')  # Setting the timezone to India (Kolkata)
+    tz = pytz.timezone('Asia/Baghdad')  # Setting the timezone to India (Kolkata)
     current_time = datetime.now(tz)
     return current_time.strftime("%Y-%m-%d %H:%M:%S %Z%z")
 
-@app.on_message(filters.command(["Time"]))
+@app.on_message(command(["Time","کات"]))
 def send_time(client, message):
     time = get_current_time()
-    client.send_message(message.chat.id, f"Current time in India: {time}")
+    client.send_message(message.chat.id, f"**کاتی کوردستان: {time}**")
