191da2bb1c894d9db2f4040596555023188fb42a
zzzdevo <110237161+zzzdevo@users.noreply.github.com>
2024-01-07 02:57:51 +0300

    Update filters.py

--- strings/filters.py
+++ strings/filters.py
@@ -1,8 +1,7 @@
 from typing import List, Union
 
 from pyrogram import Client, filters
-from pyrogram.types import Message
-from pyrogram.errors import UserNotParticipant
+
 
 
 other_filters = filters.group & ~filters.via_bot & ~filters.forwarded
@@ -15,12 +14,3 @@ def command(commands: Union[str, List[str]]):
     return filters.command(commands, "")
 
 #####################################
-
-channel = "xv7amo" # Your Channel
-async def subscription(_, __: Client, message: Message):
-    user_id = message.from_user.id
-    try: await app.get_chat_member(channel, user_id)
-    except UserNotParticipant: return False
-    return True
-    
-subscribed = filters.create(subscription)
