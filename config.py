ec15d41d78beadced005cb691aee52e61b444acf
zzzdevo <110237161+zzzdevo@users.noreply.github.com>
2024-01-07 10:38:29 +0300

    Update config.py

--- config.py
+++ config.py
@@ -50,6 +50,7 @@ GIT_TOKEN = getenv(
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/MGIMT")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/IQSUPP")
+OWNER_CHANNEL = getenv("OWNER_CHANNEL", "https://t.me/xv7amo")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
