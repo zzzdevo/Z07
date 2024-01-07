738c9335fc6606aad9d4aceafd9341dcfab8747d
zzzdevo <110237161+zzzdevo@users.noreply.github.com>
2024-01-07 23:50:10 +0300

    Update post.py

--- DAXXMUSIC/plugins/sudo/post.py
+++ DAXXMUSIC/plugins/sudo/post.py
@@ -1,15 +1,16 @@
 from pyrogram import Client, filters
 from DAXXMUSIC import app
-from DAXXMUSIC.misc import SUDOERS
+from config import OWNER_ID, BOT_USERNAME
 from pyrogram.types import Message
 
 
-@app.on_message(filters.command(["post"], prefixes=["/", ".", ""]) & SUDOERS)
+@app.on_message(filters.command(["post"], prefixes=["/", "."]) & filters.user(OWNER_ID))
 async def copy_messages(_, message):
 
     if message.reply_to_message:
       
-        destination_group_id = -1001906948158 
+        destination_group_id = -1001801583479
+ 
 
         
         await message.reply_to_message.copy(destination_group_id)
